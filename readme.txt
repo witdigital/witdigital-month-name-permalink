412cca858217b4a19129167618b04a454d0b2eeb
Ryan Mitchell <ryanmitchell@gmail.com>
2021-06-23 12:56:18 -0600

    INIT v1.0.0

---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Wit Digital Month Name Permalink ===
+Contributors: @ryanemitchell
+Tags: permalinks, monthname, monthcode
+Requires at least: 3.7.0
+Tested up to: 5.7.2
+Stable tag: 1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+Based off https://wordpress.org/plugins/month-name-permalink/ and (https://gist.github.com/rogerhub/8306875)
+Updated for current WordPress version by Wit digital June 2021.
+
+Plugin enables use of monthname (january, june) or monthcode (jan, jun) in permalinks
+
+Supports permalinks in the form of /2014/nov/23/post-name or /2014/november/23/post-name
+
+
+== Frequently Asked Questions ==
+
+= How do i use it =
+
+Navigate to Settings -> Permalinks
+
+Select "Custom Structure" and enter the following structure
+
+/%year%/%monthname%/%day%/%postname%/
+
+Save the changes
+
+== Installation ==
+
+1. Upload to the plugins directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+= 1.0 =
+Updated tp support current version of WordPress
+Forked from https://wordpress.org/plugins/month-name-permalink/
---
+++ witdigital-month-name-permalink.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Plugin Name: Wit Digital Month Name Permalinks
+ * Description: Enables use of <code>%monthcode%</code> or <code>%monthname%</code> tags in permalinks to generate a structure like <code>/2014/nov/23/post-name</code> or <code>/2014/november/23/post-name</code>
+ * Author: @ryanemitchell
+ * License: GPLv2
+ */
+
+/**
+ * Based off https://wordpress.org/plugins/month-name-permalink/ and (https://gist.github.com/rogerhub/8306875)
+ * updated for current WordPress version by Wit digital.
+ * Plugin enables use of monthname (january, june) and monthcode (jan, jun) in permalinks
+ * Supports permalinks in the form of /2014/nov/23/post-name or /2014/november/23/post-name
+ */
+
+class Month_Name_Permalink {
+
+	/**
+	 * Month Names
+	 */
+	public static $monthnames = array(
+		'january',
+		'february',
+		'march',
+		'april',
+		'may',
+		'june',
+		'july',
+		'august',
+		'september',
+		'october',
+		'november',
+		'december',
+	);
+
+	/**
+	 * Month Codes
+	 */
+	public static $monthcodes = array(
+		'jan',
+		'feb',
+		'mar',
+		'apr',
+		'may',
+		'jun',
+		'jul',
+		'aug',
+		'sep',
+		'oct',
+		'nov',
+		'dec',
+	);
+
+	/**
+	 * Registers all required hooks
+	 */
+	public static function init() {
+		add_rewrite_tag( '%monthname%', '(' . implode('|', self::$monthnames) . ')' );
+		add_rewrite_tag( '%monthcode%', '(' . implode('|', self::$monthcodes) . ')' );
+		add_rewrite_rule(
+			'^([0-9]{4})/(' . implode( '|', self::$monthnames ) . ')/([0-9]{1,2})/(.*)?',
+			'index.php?name=$matches[4]',
+			'top'
+		);
+		add_rewrite_rule(
+			'^([0-9]{4})/(' . implode( '|', self::$monthcodes ) . ')/([0-9]{1,2})/(.*)?',
+			'index.php?name=$matches[4]',
+			'top'
+		);
+
+	}
+	/**
+	 * Filters the month name and month code tags
+	 */
+	public static function filter_post_link( $permalink, $post ) {
+		if ( false === strpos( $permalink, '%monthname%' ) && false === strpos( $permalink, '%monthcode%' ) ) {
+			return $permalink;
+		}
+
+		try {
+			$monthindex = intval(get_post_time( 'n', "GMT" == false, $post->ID ));
+
+			$monthname = self::$monthnames[$monthindex - 1];
+			$monthcode = self::$monthcodes[$monthindex - 1];
+
+			$permalink = str_replace( '%monthname%', $monthname, $permalink );
+			$permalink = str_replace( '%monthcode%', $monthcode, $permalink );
+
+			return $permalink;
+		} catch (Exception $e) {
+			return $permalink;
+		}
+	}
+
+}
+
+add_action( 'init', array( 'Month_Name_Permalink', 'init' ) );
+add_filter( 'post_link', array( 'Month_Name_Permalink', 'filter_post_link' ), 10, 2 );
